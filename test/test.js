476eb22245f6de6cc05c66d36f24ca8fbe7bf7db
Dustin Martin <dm@dustinmartin.net>
2013-08-29 19:59:22 -0500

    Add simple test to try out Travis CI

---
+++ test/test.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var chai = require("chai"),
+    expect = chai.expect;
+
+describe( "The test", function(){
+
+    it("should be successful", function(){
+        expect( true ).to.equal( true );
+    });
+
+});
